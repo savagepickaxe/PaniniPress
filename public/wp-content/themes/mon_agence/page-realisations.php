21e1f01321bdc2ffd94792fdb3c7bab810a47a7b
Laurie Roy <laurie.roy4@gmail.com>
2025-03-07 10:12:32 -0500

    début du projet

--- public/wp-content/themes/mon_agence/page-realisations.php
+++ public/wp-content/themes/mon_agence/page-realisations.php
@@ -6,10 +6,11 @@ get_header();
     <main class="page">
 
         <?php //var_dump($post); //Ce que reçoit la page?>
+        <?php the_content(); ?>
         <div>
             <h2><?php the_title(); ?></h2>
         </div>
-        <?php the_content(); ?>
+        
 
         <?php
         //Requête et boucle d'affichage des articles avec ACF
@@ -26,7 +27,25 @@ get_header();
 
         if(have_posts()){
             //tant qu'il restera des articles
+            
+            
             foreach ($posts as $post){?>
+
+<?php
+
+$image_info=get_field("photo_1");
+
+    //Si l'image est définie dans ACF
+        if($image_info!=null){
+
+            //Utiliser la balise picture pour le redimensionnement de l'image ?>
+            <picture>
+                 <source media="(min-width: 800px)" srcset="<?php echo $image_info['sizes']["large"];?>">
+                 <source media="(min-width: 601px)" srcset="<?php echo $image_info['sizes']["medium"];?>">
+                <img src="<?php echo $image_info['sizes']['thumbnail'];?>" alt="<?php echo $image_info["alt"];?>">
+            </picture>
+
+<?php }?>
                 <article class="article">
                     <header class="article__entete">
                         <h2 class="article__titre">
@@ -34,21 +53,7 @@ get_header();
                             <a class="article__lien" href="<?php the_permalink();?>"><?php the_title()?></a>
                         </h2>
                     </header>
-                    <?php
-
-                    $image_info=get_field("photo_1");
-
-                    //Si l'image est définie dans ACF
-                    if($image_info!=null){
-
-                        //Utiliser la balise picture pour le redimensionnement de l'image ?>
-                        <picture>
-                            <source media="(min-width: 800px)" srcset="<?php echo $image_info['sizes']["large"];?>">
-                            <source media="(min-width: 601px)" srcset="<?php echo $image_info['sizes']["medium"];?>">
-                            <img src="<?php echo $image_info['sizes']['thumbnail'];?>" alt="<?php echo $image_info["alt"];?>">
-                        </picture>
-
-                    <?php }?>
+                   
 
                     <p><?php echo get_field("nom_client")?></p>
 
