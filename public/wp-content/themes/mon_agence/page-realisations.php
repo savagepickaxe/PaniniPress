7020cfcf291cb8c5e2490050d5623d5d85b4c793
Laurie Roy <laurie.roy4@gmail.com>
2025-03-21 08:31:33 -0400

    Ajout page réalisations

--- public/wp-content/themes/mon_agence/page-realisations.php
+++ public/wp-content/themes/mon_agence/page-realisations.php
@@ -1,75 +1,55 @@
+
 <?php
-/* Template name: Réalisations */
 get_header();
 ?>
 
-    <main class="page">
+<main class="page">
+    <div class="titre__realisations">
+        <h2 class="h2__realisations"> Nos <?php the_title(); ?></h2>
+    </div>
 
-        <?php //var_dump($post); //Ce que reçoit la page?>
-        <?php the_content(); ?>
-        <div>
-            <h2><?php the_title(); ?></h2>
-        </div>
-        
+    <?php
+    $posts = get_posts(array(
+        'posts_per_page' => -1,
+        'post_type' => 'realisations',
+        'post_status' => 'publish',
+        'orderby' => 'the_title',
+        'order' => 'ASC',
+    ));
 
-        <?php
-        //Requête et boucle d'affichage des articles avec ACF
-        //À mettre dans les pages utilisant les articles personnalisés et adapter****************
-        $posts = get_posts(array(
-            'posts_per_page' => -1,
-            'post_type'	=> 'realisations',
-            'post_status' => 'publish',
-            'orderby' => 'the_title',
-            'order' => 'ASC',
-        ));
+    if (!empty($posts)) {
+        $count = 0;
+        foreach ($posts as $post) {
+            $image_info = get_field("photo_1"); ?>
 
-       // var_dump($posts);
+            <div class="realisations-container">
+                <?php if ($image_info) { ?>
+                    <picture class="article__image">
+                        <source media="(min-width: 800px)" srcset="<?php echo esc_url($image_info['sizes']['large']); ?>">
+                        <source media="(min-width: 601px)" srcset="<?php echo esc_url($image_info['sizes']['medium']); ?>">
+                        <img src="<?php echo esc_url($image_info['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image_info["alt"]); ?>">
+                    </picture>
+                <?php } ?>
 
-        if(have_posts()){
-            //tant qu'il restera des articles
-            
-            
-            foreach ($posts as $post){?>
-
-<?php
-
-$image_info=get_field("photo_1");
-
-    //Si l'image est définie dans ACF
-        if($image_info!=null){
-
-            //Utiliser la balise picture pour le redimensionnement de l'image ?>
-            <picture>
-                 <source media="(min-width: 800px)" srcset="<?php echo $image_info['sizes']["large"];?>">
-                 <source media="(min-width: 601px)" srcset="<?php echo $image_info['sizes']["medium"];?>">
-                <img src="<?php echo $image_info['sizes']['thumbnail'];?>" alt="<?php echo $image_info["alt"];?>">
-            </picture>
-
-<?php }?>
-                <article class="article">
+                <article class="article article__content">
                     <header class="article__entete">
                         <h2 class="article__titre">
-                            <?php //affiche le lien et le titre de l'article'?>
-                            <a class="article__lien" href="<?php the_permalink();?>"><?php the_title()?></a>
+                            <a class="article__lien" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                         </h2>
                     </header>
-                   
 
-                    <p><?php echo get_field("nom_client")?></p>
-
+                    <p class="nom__client"><?php echo get_field("nom_client"); ?></p>
                     <p class="article__texte">
-                        <?php //affiche le l'extrait de la réalisation
-                        the_excerpt();
-                        ?>
+                        <?php the_excerpt(); ?>
                     </p>
+                    <a href="<?php the_permalink(); ?>" class="bouton__realisations">En savoir plus</a>
                 </article>
-            <?php }
+            </div>
 
-            //réinitialise les données reçues par défaut du gabarit pour afficher le
-            //reste des informations de la page, s'il y a lieu
-            //wp_reset_postdata();
-        }?>
+        <?php $count++;
+        }
+    } ?>
+</main>
 
-    </main>
 
 <?php get_footer()?>
